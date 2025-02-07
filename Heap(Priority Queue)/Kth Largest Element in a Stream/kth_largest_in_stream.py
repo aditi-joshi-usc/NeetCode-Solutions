ad329dfa7c48489729eb3d86ddfb3b77ea6ede70
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-02-06 19:48:19 -0800

    Create kth_largest_in_stream.py

---
+++ Heap(Priority Queue)/Kth Largest Element in a Stream/kth_largest_in_stream.py
@@ -0,0 +1,16 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums_heap = nums
+        heapq.heapify(self.nums_heap)
+        while len(self.nums_heap) > k:
+            heapq.heappop(self.nums_heap)
+        
+
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums_heap, val)
+        if len(self.nums_heap) > self.k:
+            heapq.heappop(self.nums_heap)
+        return self.nums_heap[0]
