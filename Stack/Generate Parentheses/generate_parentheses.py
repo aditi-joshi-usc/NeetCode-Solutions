713383b28e662d6e195484fc8b62ef8af1974298
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-02-17 17:03:41 -0800

    Create generate_parentheses.py

---
+++ Stack/Generate Parentheses/generate_parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = []
+        stack = []
+
+        def dfs(openB, closeB):
+            if openB == closeB and openB == n:
+                res.append(''.join(stack))
+                return 
+            
+            if openB< n:
+                stack.append('(')
+                dfs(openB+1, closeB)
+                stack.pop()
+            if closeB<openB:
+                stack.append(')')
+                dfs(openB, closeB+1)
+                stack.pop()
+        
+        
+        dfs(0,0)
+        return res
+
+
