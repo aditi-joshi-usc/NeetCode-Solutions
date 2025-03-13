9aa2284183f7ef29364c17ac38a35c8b9e52d410
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-13 12:06:18 -0700

    Update word_break.py

--- 1-D Dynamic Programming/Word Break/word_break.py
+++ 1-D Dynamic Programming/Word Break/word_break.py
@@ -14,3 +14,23 @@ class Solution:
 
         #Time Complexity = O(len(s) * len(word_dict) * max_len of word in dict) = O(n*m*t)
         #Space Complexity = O(n)
+
+
+
+# Using Hashset
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n+1)
+        words = set(wordDict)
+        dp[0] = True
+
+        for i in range(1, (n+1)):
+            for j in range(i):
+                if dp[j] and s[j:i] in words:
+                    dp[i] = True
+                    break
+        return dp[n]
+
+#Time Complexity = O(n^2)
+#Space Complexity = O(n)
